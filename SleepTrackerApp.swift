3798a8c54e5c06258f3173ed911917e531bc3de8
DanyloBohynia <danilabogynia@gmail.com>
2024-05-26 19:01:11 -0400

    Initial commit of Sleep Tracker app

---
+++ SleepTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  SleepTrackerApp.swift
+//  SleepTracker
+//
+//  Created by danqya on 26/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SleepTrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
